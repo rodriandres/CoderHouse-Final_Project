63a093c7a05d5539b1a05cc47d8b24d201f2bc6d
rodriandres <rodriandres998@gmail.com>
2022-01-01 17:35:39 -0300

    health power fix 1

--- Assets/Scripts/Lifes/PlayerLife.cs
+++ Assets/Scripts/Lifes/PlayerLife.cs
@@ -2,21 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class PlayerLife : MonoBehaviour
 {
     [SerializeField] private GameObject heartsPanel;
     [SerializeField] private int lifes;
-
     [SerializeField] private bool dead;
-
-
     private void Start()
     {
         //Transform lifePanel = heartsPanel.gameObject.transform.Find("LifesPanel");
         //lifes = lifePanel.;
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -26,43 +21,34 @@ public class PlayerLife : MonoBehaviour
             // Set Dead Event
         }
     }
-
     public void TakeDamage(int dmg)
     {
         Transform lifePanel = transform.Find("LifesPanel");
-
         if (lifes >= 1 && !dead)
         {
             for (int i = 0; i < dmg; i++)
             {
-                lifes -= dmg;
-
+                lifes -= 1;
                 int max = lifePanel.childCount;
-
                 lifePanel.GetChild(max).gameObject.SetActive(false);
             }
         }
         else
         {
-            dead = true;    
+            dead = true;
         }
     }
-
     public void HealLife(int healValue)
     {
         Transform lifePanel = transform.Find("LifesPanel");
         Debug.Log(lifePanel);
-
         if (lifes >= 1)
         {
             lifes += healValue;
             for (int i = 0; i < healValue; i++)
             {
-                
-
                 int max = lifePanel.childCount;
-
-                lifePanel.GetChild(max + 1).gameObject.SetActive(true);
+                lifePanel.GetChild(max - 1).gameObject.SetActive(true);
             }
         }
     }
