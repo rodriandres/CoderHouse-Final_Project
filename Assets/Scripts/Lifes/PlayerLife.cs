702b4ace8f294560199d91c381c6432a6c0a2656
Sergio Diaz <sergio92diaz@gmail.com>
2022-01-03 01:04:56 -0300

    Take Damage Fix
    (Sigue haciendo dos veces daño)

--- Assets/Scripts/Lifes/PlayerLife.cs
+++ Assets/Scripts/Lifes/PlayerLife.cs
@@ -64,41 +64,31 @@ public class PlayerLife : MonoBehaviour
             int actives = 0;
             lifes -= dmg;
             int max = lifePanel.childCount;
-
             for (int i = 0; i < max; i++)
             {
                 if (lifePanel.GetChild(i).gameObject.activeSelf)
                 {
                     actives += 1;
                 }
-
             }
-
             int containernumber = 0;
-
             for (int i = 0; i < max; i++)
-            {   
-                if (containernumber < actives)
+            {
+                if (containernumber == actives - 1)
+                {
+                    lifePanel.GetChild(containernumber).gameObject.SetActive(false);
+                    actives -= 1;
+                }
+                else
                 {
                     containernumber += 1;
                 }
-
-                else
-                {
-                    lifePanel.GetChild(max - actives).gameObject.SetActive(false);
-                    actives -= 1;
-                }
-                
             }
-
-
         }
         else
         {
             dead = true;
         }
-
-        
     }
     public void HealLife(int healValue)
     {
